bac87838c444d19103ca1f645dcec662ddd5e8dd
Queennothing33 <gparejas23@outlook.com>
2025-03-24 17:30:26 -0300

    botão salvar
    Adicionar botão de cadastrar o produto

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -1,19 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+import java.sql.SQLException;
 
 public class ProdutosDAO {
     
@@ -23,20 +13,31 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        conn = new conectaDAO().connectDB();
+         String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+     
+         try {
+             prep = conn.prepareStatement(sql);
+             prep.setString(1, produto.getNome());
+             prep.setInt(2, produto.getValor());
+             prep.setString(3, produto.getStatus());
+             prep.executeUpdate();
+             JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+         } catch (SQLException erro) {
+             JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + erro.getMessage());
+         } finally {
+         try {
+            prep.close();
+            conn.close();
+         } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao fechar conexão: " + e.getMessage());
+         }
+    }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
         return listagem;
-    }
-    
-    
-    
-        
+    }      
 }
 
